d8f45436a5b8561766093f42c47eb059f447669e
Gökhan SARIGÜL <gokhan@DESKTOP-FCMC2EN>
2023-06-14 08:54:15 +0300

    Active..

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin');
+            $table->boolean('active');
             $table->rememberToken();
             $table->timestamps();
         });
